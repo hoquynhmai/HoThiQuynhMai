77a7e7d81578424e1363c2293e1f5d526454de07
hoquynhmai <hoquynhmai1505@gmail.com>
2020-08-21 09:59:34 +0700

    up bai

--- Module2/src/bai14_thuat_toan_sap_xep/bai_tap/minh_hoa_sap_xep_chen/InsertionSortByStep.java
+++ Module2/src/bai14_thuat_toan_sap_xep/bai_tap/minh_hoa_sap_xep_chen/InsertionSortByStep.java
@@ -16,25 +16,37 @@ public class InsertionSortByStep {
         for (int i = 0; i < list.length; i++) {
             System.out.print(list[i] + " ");
         }
-        System.out.println("\nSau khi sắp xếp chèn: ");
+        System.out.println("\n---------Strart Insertion Sort--------------");
         insertionSortByStep(list);
     }
 
     public static void insertionSortByStep(int[] list) {
+        boolean needNextPass = true;
         int j = 0;
-        for (int i = 0; i < list.length; i++) {
-            int key = list[i];
-            j = i - 1;
-            while (j >= 0 && list[j] > key) {
-                if(list[j] > list[j+1])
-                System.out.println("Di chuyển " + list[j] + " Lùi về sau 1 vị trí để chèn " + list[j+1]);
-                list[j + 1] = list[j];
-                j = j - 1;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length; i++) {
+                int key = list[i];
+                j = i - 1;
+
+                while (j >= 0 && list[j] > key) {
+                    if (list[j] > list[j + 1])
+                        System.out.println("Di chuyển " + list[j] + " Lùi về sau để chèn " + list[j + 1]);
+                    list[j + 1] = list[j];
+                    j = j - 1;
+                    list[j + 1] = key;
+                    needNextPass = true;
+                }
             }
-            list[j + 1] = key;
-        }
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i] + " ");
+            if (needNextPass == false) {
+                System.out.println("Mảng có thể được sắp xếp và không cần chuyển tiếp");
+                break;
+            }
+            System.out.println("Danh sách sau khi sắp xếp chèn: ");
+            for (int l = 0; l < list.length; l++) {
+                System.out.print(list[l] + " ");
+            }
+            System.out.println();
         }
     }
 }
